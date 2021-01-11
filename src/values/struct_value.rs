fb1b2278b5646007bf0f2fc22d9f922260066616
Alastair Reid <adreid@google.com>
2021-01-11 15:00:53 +0000

    Add get[_num]_operand to StructValue

--- src/values/struct_value.rs
+++ src/values/struct_value.rs
@@ -1,4 +1,6 @@
 use llvm_sys::prelude::LLVMValueRef;
+use llvm_sys::core::{LLVMGetOperand, LLVMGetNumOperands};
+use crate::values::{AnyValueEnum};
 
 use std::ffi::CStr;
 
@@ -51,6 +53,30 @@ impl<'ctx> StructValue<'ctx> {
     pub fn replace_all_uses_with(self, other: StructValue<'ctx>) {
         self.struct_value.replace_all_uses_with(other.as_value_ref())
     }
+
+    pub fn get_num_operands(self) -> u32 {
+        unsafe {
+            LLVMGetNumOperands(self.as_value_ref()) as u32
+        }
+    }
+
+    pub fn get_operand(self, index: u32) -> Option<AnyValueEnum<'ctx>> {
+        let num_operands = self.get_num_operands();
+
+        if index >= num_operands {
+            return None;
+        }
+
+        let operand = unsafe {
+            LLVMGetOperand(self.as_value_ref(), index)
+        };
+
+        if operand.is_null() {
+            return None;
+        }
+        Some(AnyValueEnum::new(operand))
+    }
+
 }
 
 impl AsValueRef for StructValue<'_> {
