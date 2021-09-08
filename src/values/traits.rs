90731fe8ce585ef2af4caa31190ef05e6d6d1042
Thierry Marianne <thierry@marianne.io>
2021-09-08 11:19:59 +0200

    Add missing unsafe keywords

--- src/values/traits.rs
+++ src/values/traits.rs
@@ -161,7 +161,10 @@ pub trait AnyValue<'ctx>: AsValueRef + Debug {
         if operand.is_null() {
             return None;
         }
-        Some(AnyValueEnum::new(operand))
+
+        unsafe {
+            Some(AnyValueEnum::new(operand))
+        }
     }
 }
 
